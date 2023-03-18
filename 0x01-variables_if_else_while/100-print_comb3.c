9957b315d643e5076aa1b465f3069951564c7f2f
kukuackah <ackahtic1@yahoo.com>
2023-03-18 04:46:24 +0000

    resolved error

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,22 +9,13 @@ int main(void)
 	int n;
 	int m;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 0; n < 10; n++)
 	{
-		for (m = 48; m <= 57; m++)
+		for (m = n + 1; m < 10; m++)
 		{
-			putchar(n);
-			putchar(m);
-		}	int f;
-	int s;
-
-	for (f = 0; f < 10; f++)
-	{
-		for (s = f + 1; s < 10; s++)
-		{
-			putchar(48 + f);
-			putchar(48 + s);
-			if (f == 8 && s == 9)
+			putchar(48 + n);
+			putchar(48 + m);
+			if (n == 8 && m == 9)
 			{}
 			else
 			{
@@ -34,6 +25,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	}
 	return (0);
 }
