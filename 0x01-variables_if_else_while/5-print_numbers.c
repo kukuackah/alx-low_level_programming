952adfca25e52850f183228b10a8cbabaf0b6cf3
kukuackah <ackahtic1@yahoo.com>
2023-03-18 02:35:00 +0000

    program that print numbers from 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	printf("\n");
+	return (0);
+}
