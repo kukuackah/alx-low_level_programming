d3fc5dc89a56a39b12e85243a964ce1480b5f7ef
kukuackah <ackahtic1@yahoo.com>
2023-03-18 02:50:21 +0000

    writing number zero to nine without using char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int dig;
+
+	for (dig = 0; dig <= 10; dig++)
+{
+	putchar(48 + dig);
+}
+	putchar('\n');
+	return (0);
+}
