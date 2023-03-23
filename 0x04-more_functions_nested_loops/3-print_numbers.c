c81b32f4b6c11c915e66ce269f81d8e3a198b9fd
kukuackah <ackahtic1@yahoo.com>
2023-03-23 23:47:20 +0000

    function that prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers 0 to 9
+ * @c: main fuction name
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}	
