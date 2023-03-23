1dd38c3c2a3633a9da81b16dbcaf274f8f7bf52a
kukuackah <ackahtic1@yahoo.com>
2023-03-23 23:51:07 +0000

    betty resolved

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,13 @@
  * Return: void
  */
 void print_most_numbers(void)
-{	
+{
 	char c;
 
 	for (c = '0' ; c <= '9' ; c++)
 	{
-	       if (!(c == '2' || c == '4'))
-		       _putchar(c);
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
 	_putchar('\n');
-}	       
+}
