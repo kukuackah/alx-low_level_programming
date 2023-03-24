cbd6ba521c00eb187f1724aa968b3424bb2fbf94
kukuackah <ackahtic1@yahoo.com>
2023-03-24 00:47:30 +0000

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - print a line
+ * @n: number of time the character is printed
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = -1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
