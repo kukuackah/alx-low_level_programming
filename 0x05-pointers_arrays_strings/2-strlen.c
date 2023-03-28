a883aa6acd359c61c1fb23cce2d2f53fb45f0c50
kukuackah <ackahtic1@yahoo.com>
2023-03-28 01:10:03 +0000

    resolved

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,14 +5,14 @@
  * Return: 0 always
  */
 int _strlen(char *s)
- {
- 	int size;
+{
+	int size;
 
- 	size = 0;
- 	while (s[size])
- 	{
- 		size++;
- 	}
+	size = 0;
+	while (s[size])
+	{
+		size++;
+	}
 
- 	return (size);
- }
+	return (size);
+}
